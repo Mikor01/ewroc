aa579562765165281503294c83087ffb8d5c02ae
Mikor01 <36859179+Mikor01@users.noreply.github.com>
2020-01-28 00:09:51 +0100

    Add files via upload

---
+++ pliki/index.php
@@ -0,0 +1,153 @@
+﻿<html>
+   <head>
+      <!--  <meta charset="UTF-8">
+         <meta http-equiv="refresh" content="1;url=http://example.com">
+         <script type="text/javascript">
+             window.location.href = "http://poradnia4.krakow.pl/wordpress"
+         </script>
+         <title>Page Redirection</title>
+         -->
+      <link href="style.css" type="text/css" rel="stylesheet" />
+      <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+      <META NAME="konwerter" CONTENT="Ogonki97 1.3">
+      <link href="nav.css" rel="stylesheet" type="text/css" />
+      <!--This is the nacigation bar's stylesheet. Copy it to the <head></head> area of your HTML and  PHP files.-->
+      <link href="examples.css" rel="stylesheet" type="text/css" />
+      <!--This is used for the example files only. You don't need it on your website.-->
+      <script type="text/javascript">
+         document.createElement('nav');
+      </script>
+      <!--This line of javascript is an Internet Explorer Fix.-->
+   </head>
+   <body topmargin=0>
+      <center>
+      <table cellspacing=0 cellpadding=0 border=0 width=100% height=100%>
+         <tr>
+            <td bgcolor=red class=header><?php include('cytat.php'); ?>
+         </tr>
+         <tr>
+            <td bgcolor=red class=linia>
+         </tr>
+         <tr>
+            <td bgcolor=red class=header2 align=center><a href=poradnia.php><img src=images/poradnia.jpg></a></td>
+         </tr>
+         <tr>
+            <td bgcolor=red class=header3>
+         </tr>
+         <tr>
+            <td bgcolor=white >
+               <center>
+               <table border =0 cellspacing=0 cellpadding=0 border=0 width=1000>
+                  <tr>
+                     <td colspan=3></td>
+                  </tr>
+                  <tr>
+                     <td border =1 valign=top class=menu>
+                        <?php
+                           include("menu.php");
+                           ?>
+                     </td>
+                     <td bgcolor=white width=600 class=srodek>
+                        <center>
+						                        <br
+                        <br>
+						       <br><br>
+ <br>
+Zakończył się konkurs plastyczny realizowany przez Zespół ds. Działań Lokalnych nr 4 <br>we współpracy z
+
+Radą Dzielnicy XVIII Krakowa pod hasłem (prze)MOC W RODZINIE. <br><br><a href=pliki/wyniki.docx><font class=x color=red><b>Wyniki konkursu - do pobrania</b></font></a><br>
+
+                           <br><br><hr><br>
+                           <font class=x color=red><b>UWAGA !!!</b></font><br><br>
+<b>INFOLINIA - dla Rodziców Dzieci Sześcioletnich !!</b><br>
+ <br>
+W ramach wsparcia dla rodziców, w uzyskaniu wiedzy, informacji <br>
+pozwalających na podjęcie odpowiedzialnej decyzji, <br>
+czy posłać dziecko do pierwszej klasy, <br>
+czy też jeszcze przez rok być rodzicem przedszkolaka,<br>
+uruchomiona zostaje INFOLINIA <br>
+- rodzaj Telefonu Zaufania dla rodziców dzieci sześcioletnich.<br>
+Linię obsługiwać będą <br>
+pracownicy krakowskich poradni psychologiczno - pedagogicznych<br>
+według zamieszczonego niżej harmonogramu:<br>
+
+                           <br><br>
+                        <table cellspacing=0 cellpadding=0 border=1>
+                           <tr>
+                              <td class=normal>Data dyżuru telefonicznego</td>
+							  <td class=normal>Nazwa Poradni obsługującej<br> Infolinię</td>
+							  <td class=normal>Godziny dyżuru telefonicznego</td>
+							  <td class=normal>Numer infolinii</td>
+							  <td class=normal>Specjalizacja osoby dyżurującej</td>
+                           </tr>
+						   <tr>
+                              <td class=normal>15-17 lutego 2016r.</td>
+							  <td class=normal>Poradnia Psychologiczno-Pedagogiczna nr 1</td>
+							  <td class=normal>15.02.-godz. 15-17<br>17.02.: godz.15-17<br>19.02.: godz.10-12</td>
+							  <td class=normal>124305367<br> 6600789</td>
+							  <td class=normal>psycholog</td>
+                           </tr>
+						   <tr>
+                              <td class=normal>22-26 lutego 2016r.</td>
+							  <td class=normal>Poradnia Psychologiczno-Pedagogiczna nr 2</td>
+							  <td class=normal>22.02: godz.15-17<br>24.02.: godz.15-17<br>26.02.: godz.10-12</td>
+							  <td class=normal>124156968<br>660637504</td>
+							  <td class=normal>psycholog</td>
+                           </tr>
+						   <tr>
+                              <td class=normal>29 lutego-04 marca 2016r.</td>
+							  <td class=normal>Poradnia Psychologiczno-Pedagogiczna nr 3</td>
+							  <td class=normal>29.02.: godz.15-17<br>02.03.: godz.15-17<br>04.03.: godz.10-12</td>
+							  <td class=normal>122661950<br>660637242</td>
+							  <td class=normal>psycholog</td>
+                           </tr>
+						   <tr>
+                              <td class=normal>07-11 marca 2016r.</td>
+							  <td class=normal>Poradnia Psychologiczno-Pedagogiczna nr 4</td>
+							  <td class=normal>07.03.: godz.15-17<br>09.03.: godz.15-17<br>11.03.: godz.10-12</td>
+							  <td class=normal>126441885<br>660637415</td>
+							  <td class=normal>psycholog</td>
+                           </tr>
+						   <tr>
+                              <td class=normal>14-18 marca 2016r.</td>
+							  <td class=normal>Specjalistyczna Poradnia Wczesnej Pomocy <br>Psychologiczno-Pedagogicznej</td>
+							  <td class=normal>14.03.: godz.15-17<br>16.03.: godz.15-17<br>18.03.: godz.10-12</td>
+							  <td class=normal>124121566<br>660637360</td>
+							  <td class=normal>psycholog</td>
+                           </tr>
+						   <tr>
+                              <td class=normal>21-25 marca 2016r.</td>
+							  <td class=normal>Specjalistyczna Poradnia Psychologiczno-Pedagogiczna <br>dla Dzieci z Niepowodzeniami Edukacyjnymi</td>
+							  <td class=normal>21.03.: godz.15-17<br>23.03.: godz.15-17<br>25.03.: godz.10-12</td>
+							  <td class=normal>124224383<br>660637312</td>
+							  <td class=normal>psycholog<br>pedagog</td>
+                           </tr>
+						   <tr>
+                              <td class=normal>29 marca -1 kwietnia 2016r.</td>
+							  <td class=normal>Specjalistyczna Poradnia Psychologiczno-Pedagogiczna <br>dla Dzieci w Wieku Przedszkolnym</td>
+							  <td class=normal>30.03.: godz.15-17<br>01.04.: godz.10-12</td>
+							  <td class=normal>126328773</td>
+							  <td class=normal>psycholog<br>pedagog</td>
+                           </tr>
+						   </table>
+                        <br><br>
+                        <br><br>
+                     </td>
+                     <td class=content width=100></td>
+                  </tr>
+               </table>
+            </td>
+         </tr>
+         <tr>
+            <td>
+               <?php
+                  include("linki.php");
+                  ?>
+            </td>
+         </tr>
+         <tr>
+            <td class=footer></td>
+         </tr>
+      </table>
+   </body>
+</html>
---
+++ pliki/update_query.sql
@@ -0,0 +1,19 @@
+UPDATE `aktualnosci` SET `akt_tresc`= '<p class="MsoNormal" style="margin-bottom: 0.0001pt; text-align: center;" align="center"><strong>Poradnia Psychologiczno- Pedagogiczna nr 4 w Krakowie</strong></p>
+<p class="MsoNormal" style="margin-bottom: 0.0001pt; text-align: center;" align="center"><strong>Wydzial Edukacji</strong></p>
+<p class="MsoNormal" style="margin-bottom: 0.0001pt; text-align: center;" align="center"><strong>Urzedu Miasta Krakowa&nbsp;</strong></p>
+<p class="MsoNormal" style="margin-bottom: 0.0001pt; text-align: center;" align="center">zapraszaja na konferencje:</p>
+<p class="MsoNormal" style="margin-bottom: 0.0001pt; text-align: center;" align="center">&nbsp;pod patronatem</p>
+<p class="MsoNormal" style="margin-bottom: 0.0001pt; text-align: center;" align="center"><strong>Pani Katarzyny Cieciak</strong></p>
+<p class="MsoNormal" style="margin-bottom: 0.0001pt; text-align: center;" align="center">Zastepcy Prezydenta Miasta Krakowa</p>
+<p class="MsoNormal" style="margin-bottom: 0.0001pt; text-align: center;" align="center">ds. Edukacji i Sportu</p><br>
+<p class="MsoNormal" style="margin-bottom: 0.0001pt; text-align: left;" align="center"><strong>Celem konferencji</strong>&nbsp;jest uwrazliwienie nauczycieli na potrzeby dzieci, kt&oacute;re rozpoczely nauke w wieku szesciu lat, a obecnie rozpoczynaja II etap edukacji. &nbsp;</p>
+<p class="MsoNormal" style="margin-bottom: 0.0001pt; text-align: left;" align="center">Do udzialu w konferencji w spos&oacute;b szczeg&oacute;lny, zapraszamy nauczycieli i wychowawc&oacute;w klas IV-VI szk&oacute;l podstawowych.&nbsp;</p><br>
+<p class="MsoNormal" style="margin-bottom: 0.0001pt; text-align: center;" align="center">Konferencja odbedzie sie</p>
+<p class="MsoNormal" style="margin-bottom: 0.0001pt; text-align: center;" align="center"><strong>26 marca 2015 roku w godz. 9:00-14:30</strong></p>
+<p class="MsoNormal" style="margin-bottom: 0.0001pt; text-align: center;" align="center">w Sali Obrad Rady Miasta Krakowa</p>
+<p class="MsoNormal" style="margin-bottom: 0.0001pt; text-align: center;" align="center">im. Stanislawa Wyspianskiego</p>
+<p class="MsoNormal" style="margin-bottom: 0.0001pt; text-align: center;" align="center">Plac Wszystkich Swietych 3 &ndash; 4</p><br>
+<p>Osoby uczestniczace otrzymaja materialy konferencyjne oraz zaswiadczenia.</p>
+<p>Prosimy o zglaszanie udzialu najp&oacute;zniej do&nbsp;<strong>23 marca 2015 r.</strong> na: <a href=mailto:wkasporadnia@wp.pl><font size=12>wkasporadnia@wp.pl</font></a> <br>(w tytule wiadomosci wpisac: &bdquo;Konferencja 6-latek&rdquo;) - decyduje kolejnosc zgloszen</p>
+<p>Do pobrania agenda konferencji: <a href="pliki/agenda.docx">AGENDA</a></p>
+<p>&nbsp;</p>' where `akt_data` > curdate()
